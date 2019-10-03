d58986825d271d5d9ce03f1d23565a38fc267bdd
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-10-03 10:21:12 -0500

    add comments

--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class ContactsApplication {
     public static void main(String[] args) {
 
+
         List<Contact> contactList = loadcontacts();
         boolean leave = false;
         do {
+            // print menu returns an integer. Switch statement evaluates the integer and executes the appropriate case.
 
             switch (printMenu()) {
                 case 1:
@@ -36,11 +38,18 @@ public class ContactsApplication {
         updateContact(contactList);
     }
 
+    /*
+        Finds contacts.txt in the data directory and reads all the files, and converts the contacts from strings to objects,
+         and returns the list of contact objects.
+
+
+     */
     public static List<Contact> loadcontacts() {
         List<Contact> contacts = new ArrayList<>();
         Path path = Paths.get("data", "contacts.txt");
         try {
             List<String> bufferList = Files.readAllLines(path);
+            // goes through list of strings and converts them to contact objects
             for (String line : bufferList) {
                 String[] split = line.split("/");
                 Contact contact = new Contact(split[0], split[1]);
@@ -53,7 +62,9 @@ public class ContactsApplication {
 
         return contacts;
     }
-
+/*
+    This method prints the menu. It returns an integer that represents the users choice.
+ */
     public static int printMenu() {
         Input input = new Input();
         return input.getInt(1, 5, "1. View contacts.\n" +
@@ -65,6 +76,10 @@ public class ContactsApplication {
 
     }
 
+    /*
+        This method allows you to view the contacts.  It takes in a list of contact objects as the parameter.
+         Uses for each loop to print out each individual element.
+     */
     public static void viewContacts(List<Contact> contactList) {
         System.out.println("Name | Phone number");
         System.out.println("---------------");
@@ -74,6 +89,11 @@ public class ContactsApplication {
 
         }
     }
+
+    /*
+        This method adds a new  contact. It takes in a list of contact objects as the parameter,
+         and gets the new contact name and number from user input.
+     */
 
     public static List<Contact> addContact(List<Contact> contactList) {
         Input input = new Input();
@@ -87,6 +107,13 @@ public class ContactsApplication {
         return contactList;
     }
 
+
+    /*
+        This method searches the contact list for a specific name.  User search gets a name in the form of a string,
+        and the for loop goes through the contact list and checks if the user search is equal to a name in the contact list, and prints out the name and the number.
+     */
+
+
     public static void searchContact(List<Contact> contactList) {
         Input input = new Input();
         String userSearch = input.getString("Search a name");
@@ -96,6 +123,12 @@ public class ContactsApplication {
             }
         }
     }
+
+    /*
+        This method allows you to delete a contact by getting the user input in a the form of a string,
+        and uses a for each loop to check if the user input is equal to a name in the contact list.
+        If user input equals name in the contact list, then delete it by the index of that contact.
+     */
 
     public static List<Contact> deleteContact(List<Contact> contactList) {
         Input input = new Input();
@@ -113,14 +146,21 @@ public class ContactsApplication {
         return contactList;
     }
 
+    /*
+        This method updates the contact list by finding contacts.txt in the data directory.
+        Creates a list of strings called bufferlist to convert contact objects to strings.
+        Uses for each to go through contact list and adds contact name and number to the bufferlist.
+        then it writes to contacts.txt.
+     */
+
     public static void updateContact(List<Contact> contactList) {
         Path path = Paths.get("data", "contacts.txt");
         try {
-            List<String> bufferContact = new ArrayList<>();
+            List<String> bufferList = new ArrayList<>();
             for (Contact contact : contactList) {
-                bufferContact.add(contact.getName() + "/" + contact.getNumber());
+                bufferList.add(contact.getName() + "/" + contact.getNumber());
             }
-            Files.write(path, bufferContact);
+            Files.write(path, bufferList);
         } catch (IOException e) {
             e.printStackTrace();
         }
