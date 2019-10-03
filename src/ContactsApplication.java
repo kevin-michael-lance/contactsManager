2da05f3e872dd62c85f84da0488512524999b9ef
Kevin Arnall <kevinjarnall@gmail.com>
2019-10-03 11:00:44 -0500

    code cleanup

--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -1,4 +1,3 @@
-import javax.imageio.IIOException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,33 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         List<Contact> contactList = loadcontacts();
         boolean leave = false;
+
         do {
             // print menu returns an integer. Switch statement evaluates the integer and executes the appropriate case.
-
             switch (printMenu()) {
                 case 1:
                     viewContacts(contactList);
                     break;
-
                 case 2:
-                    contactList = addContact(contactList);
+                    addContact(contactList);
                     break;
                 case 3:
                     searchContact(contactList);
                     break;
                 case 4:
-                    contactList = deleteContact(contactList);
+                    deleteContact(contactList);
                     break;
                 case 5:
                     leave = true;
                     break;
             }
-
         } while (!leave);
         updateContact(contactList);
     }
@@ -41,8 +38,6 @@ public class ContactsApplication {
     /*
         Finds contacts.txt in the data directory and reads all the files, and converts the contacts from strings to objects,
          and returns the list of contact objects.
-
-
      */
     public static List<Contact> loadcontacts() {
         List<Contact> contacts = new ArrayList<>();
@@ -62,9 +57,10 @@ public class ContactsApplication {
 
         return contacts;
     }
-/*
-    This method prints the menu. It returns an integer that represents the users choice.
- */
+
+    /*
+        This method prints the menu. It returns an integer that represents the users choice.
+     */
     public static int printMenu() {
         Input input = new Input();
         return input.getInt(1, 5, "1. View contacts.\n" +
@@ -73,7 +69,6 @@ public class ContactsApplication {
                 "4. Delete an existing contact.\n" +
                 "5. Exit.\n" +
                 "Enter an option (1, 2, 3, 4 or 5):");
-
     }
 
     /*
@@ -86,7 +81,6 @@ public class ContactsApplication {
 
         for (Contact contact : contactList) {
             System.out.println(contact.getName() + " | " + contact.getNumber());
-
         }
     }
 
@@ -94,8 +88,7 @@ public class ContactsApplication {
         This method adds a new  contact. It takes in a list of contact objects as the parameter,
          and gets the new contact name and number from user input.
      */
-
-    public static List<Contact> addContact(List<Contact> contactList) {
+    public static void addContact(List<Contact> contactList) {
         Input input = new Input();
         String name = input.getString("Whats the name");
         String number = input.getString("Whats the number");
@@ -103,8 +96,6 @@ public class ContactsApplication {
         Contact contact = new Contact(name, number);
 
         contactList.add(contact);
-
-        return contactList;
     }
 
 
@@ -112,8 +103,6 @@ public class ContactsApplication {
         This method searches the contact list for a specific name.  User search gets a name in the form of a string,
         and the for loop goes through the contact list and checks if the user search is equal to a name in the contact list, and prints out the name and the number.
      */
-
-
     public static void searchContact(List<Contact> contactList) {
         Input input = new Input();
         String userSearch = input.getString("Search a name");
@@ -129,8 +118,7 @@ public class ContactsApplication {
         and uses a for each loop to check if the user input is equal to a name in the contact list.
         If user input equals name in the contact list, then delete it by the index of that contact.
      */
-
-    public static List<Contact> deleteContact(List<Contact> contactList) {
+    public static void deleteContact(List<Contact> contactList) {
         Input input = new Input();
 
         String name = input.getString("What contact do you want to delete? ");
@@ -142,8 +130,6 @@ public class ContactsApplication {
         }
 
         contactList.remove(delete);
-
-        return contactList;
     }
 
     /*
@@ -152,7 +138,6 @@ public class ContactsApplication {
         Uses for each to go through contact list and adds contact name and number to the bufferlist.
         then it writes to contacts.txt.
      */
-
     public static void updateContact(List<Contact> contactList) {
         Path path = Paths.get("data", "contacts.txt");
         try {
@@ -165,5 +150,4 @@ public class ContactsApplication {
             e.printStackTrace();
         }
     }
-
 }
